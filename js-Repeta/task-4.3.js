9a0d3e55aaf5da75a7f3d4dd020d256b222e0acf
GolovinaElia <eliagolovina@ukr.net>
2020-12-14 16:37:45 +0200

    Добавляет задачи

--- js-Repeta/task-4.3.js
+++ js-Repeta/task-4.3.js
@@ -29,10 +29,49 @@ const cart = {
                 this.items.splice(i, 1);
             }
         }
+    },
+    // Очищает полностью корзину
+    // clear() {
+    //     this.items = [];
+    // },
+    // Подсчитывает полностью все цены
+    countTotalPrice() {
+        let total = 0;
+        for (const item of this.items) {
+            total += item.price * item.quantity;
+}
+        return total;
+    },
+    // Добавить через + и - в корзину товары
+    increaseQuantity(productName) {
+        for (const item of this.items) {
+            if (item.name === productName) {
+                item.quantity += 1;
+            }
+        }
+    },
+    // Убрать через + и - из корзины товаров
+    decreaseQuantity(productName) {
+       for (const item of this.items) {
+           if (item.name === productName) {
+               if (item.quantity - 1 === 0) {
+                   this.remove(productName);
+                   return;
+                }
+                item.quantity -= 1;
+            }
+        }  
     }
 };
 cart.add({ name: 'яблоко', price: 50 });
 cart.add({ name: 'лимон', price: 60});
 console.table(cart.getItems());
-cart.remove('яблоко');
+// cart.remove('яблоко');
+// cart.clear();
+// console.table(cart.getItems());
+console.log('Total:', cart.countTotalPrice());
+cart.increaseQuantity('яблоко');
+console.table(cart.getItems());
+
+cart.decreaseQuantity('лимон');
 console.table(cart.getItems());
