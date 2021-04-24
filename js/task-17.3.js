71be65965eedbe720582234c4026beeefd2fe718
GolovinaElia <eliagolovina@ukr.net>
2021-04-24 19:16:37 +0300

    Добавляет файлы

--- js/task-17.3.js
+++ js/task-17.3.js
@@ -9,3 +9,20 @@
 // Значение переменной hexColors это массив ['#f44336', '#2196f3', '#4caf50', '#ffeb3b'].
 // Объявлена переменная rgbColors.
 // Значение переменной rgbColors это массив ['244,67,54', '33,150,243', '76,175,80', '255,235,59'].
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Пиши код ниже этой строки
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
+
+ 
